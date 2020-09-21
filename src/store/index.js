6aa8c8e65ea6108e0aa79de2319f3e981d7348e8
JeromexXD <895020092@qq.com>
2020-09-21 20:30:50 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: []
   },
   mutations: {
+    setList(state, step) {
+      state.list = step
+    }
   },
   actions: {
+    async getList(context) {
+      const { data } = await axios.get('/list.json')
+      console.log(data)
+      context.commit('setList', data)
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
