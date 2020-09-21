b76620f508b03bfd1b6637263eea704dfba14f8e
JeromexXD <895020092@qq.com>
2020-09-21 22:27:55 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,104 @@
 <template>
   <div id="app">
-    <a-input placeholder="请输入任务" class="my_ipt" />
-    <a-button type="primary">添加事项</a-button>
+    <a-input
+      :value="value"
+      @change="handleChange"
+      placeholder="请输入任务"
+      class="my_ipt"
+    />
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="infoList" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox>{{ item.info }}</a-checkbox>
+        <a-checkbox
+          @change="
+            e => {
+              changeState(e, item.id)
+            }
+          "
+          :checked="item.done"
+          >{{ item.info }}</a-checkbox
+        >
         <!-- 删除链接 -->
-        <a slot="actions">删除</a>
+        <a slot="actions" @click="deleteItem(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer区域 -->
       <div slot="footer" class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{ total }}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
+          <a-button
+            :type="btnS === 'all' ? 'primary' : 'default'"
+            @click="hightLight('all')"
+            >全部</a-button
+          >
+          <a-button
+            :type="btnS === 'undone' ? 'primary' : 'default'"
+            @click="hightLight('undone')"
+            >未完成</a-button
+          >
+          <a-button
+            :type="btnS === 'done' ? 'primary' : 'default'"
+            @click="hightLight('done')"
+            >已完成</a-button
+          >
         </a-button-group>
         <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+        <a @click="cleanDone">清除已完成</a>
       </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapMutations, mapGetters } from 'vuex'
 export default {
   name: 'app',
   data() {
     return {}
   },
   computed: {
-    ...mapState(['list'])
+    ...mapState(['list', 'value', 'btnS']),
+    ...mapGetters(['total', 'infoList'])
+  },
+  methods: {
+    ...mapMutations([
+      'changeVal',
+      'addItem',
+      'removeItem',
+      'checkedBoxState',
+      'cleanDone',
+      'changeBtnStatus'
+    ]),
+    handleChange(e) {
+      // console.log(e.target.value)
+      this.changeVal(e.target.value)
+    },
+    addItemToList() {
+      if (this.value.trim().length <= 0) {
+        return this.$message.warning('输入的内容不能为空')
+      }
+      this.addItem()
+    },
+    deleteItem(id) {
+      // console.log(id)
+      this.removeItem(id)
+    },
+    changeState(e, id) {
+      // console.log(e.target.checked, id)
+      const params = {
+        done: e.target.checked,
+        id
+      }
+      this.checkedBoxState(params)
+    },
+    hightLight(s) {
+      // console.log(s)
+      this.changeBtnStatus(s)
+    }
   },
   created() {
     this.$store.dispatch('getList')
--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    list: []
+    list: [],
+    value: '',
+    btnS: 'all'
+  },
+  getters: {
+    total(state) {
+      return state.list.filter(item => item.done === false).length
+    },
+    infoList(state) {
+      if (state.btnS === 'all') return state.list
+      if (state.btnS === 'undone') return state.list.filter(item => !item.done)
+      if (state.btnS === 'done') return state.list.filter(item => item.done)
+      return state.list
+    }
   },
   mutations: {
     setList(state, step) {
       state.list = step
+    },
+    changeVal(state, step) {
+      state.value = step
+    },
+    addItem(state) {
+      const obj = {
+        id: state.list.length,
+        info: state.value,
+        done: false
+      }
+      state.list.push(obj)
+      state.value = ''
+    },
+    removeItem(state, step) {
+      state.list.splice(step, 1)
+    },
+    checkedBoxState(state, params) {
+      const { done, id } = params
+      state.list[id].done = done
+    },
+    cleanDone(state) {
+      state.list = state.list.filter(item => item.done === false)
+    },
+    changeBtnStatus(state, s) {
+      state.btnS = s
     }
   },
   actions: {
     async getList(context) {
       const { data } = await axios.get('/list.json')
-      console.log(data)
+      // console.log(data)
       context.commit('setList', data)
     }
   },
